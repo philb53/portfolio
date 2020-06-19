b5b4f60eae29e47f666a6b95a8426cd303cf066e
philb53 <41995583+philb53@users.noreply.github.com>
2020-06-19 14:14:09 +0100

    Updates

---
+++ precache-manifest.e082d39e2a74dac5ecc790064f34f2b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53267e0b686d13c50f66e6d783b92da6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a3f222c1aea95d7e05f3",
+    "url": "/portfolio/static/css/main.331a2f97.chunk.css"
+  },
+  {
+    "revision": "8dc0c582dd8f553b7b5e",
+    "url": "/portfolio/static/js/2.196c0bbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.196c0bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f222c1aea95d7e05f3",
+    "url": "/portfolio/static/js/main.3375bd1e.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "e6dbb365a36d7c53e87e40f0897a8b10",
+    "url": "/portfolio/static/media/unsplash.e6dbb365.jpg"
+  }
+]);
