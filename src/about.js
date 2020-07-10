3a8ed2771b29bf84ad6dc8a8979c0a24db0d37ea
philb53 <41995583+philb53@users.noreply.github.com>
2020-07-10 13:16:06 +0100

    update to copy

--- src/about.js
+++ src/about.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className="about-container">
     <div className="title"><span className="white">PHILIP</span><br></br><span className="white2">BAKER</span><span className="black">.</span></div>
         <div className="about-1">
-          <div className="about-1-text"> Hi, my name is Phil. I design <br></br>websites, webapps and apps. You can see some of my work below...</div>
+          <div className="about-1-text"> <span>Hi, my name is Phil. I design </span><br></br><span> websites,webapps and apps. </span><br></br><span>You can see some of my work below...</span></div>
         </div>
         <div className="about-2">
           <div className="about-2-text">Self taught full stack javascript <br></br>engineer with latest  React and node.js knowledge</div>
